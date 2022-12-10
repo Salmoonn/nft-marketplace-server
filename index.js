97b05ff4e14b9649b7772d0ff0d32721dc618cf8
Salmoonn <qwersapego@gmail.com>
2022-12-10 12:03:05 +0300

    update

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(cors());
 
-app.use('/img', express.static('database/img'));
-app.use('/avatar', express.static('database/avatars'));
+app.use('/i', express.static('database/img'));
+app.use('/a', express.static('database/avatars'));
 app.use('/other', express.static('database/other'));
 app.use('/placeholder', express.static('database/placeholder'));
 app.use('/d', express.static('database/description'));
@@ -16,15 +16,20 @@ app.use('/d', express.static('database/description'));
 
 app.get('/favicon.ico', (req, res) => res.end())
 
-app.get('/unit/*', (req, res) => {
-  res.send(require('./database/unit/' + req.params[0]))
+app.get('/unit/:id', (req, res) => {
+  res.send(require('./database/unit/' + req.params.id))
 })
 
-app.get('/topcreator/*', (req, res) => {
-  const login = require('./database/users/topcreator.json')[req.params[0]]
-  const topcreator = require('./database/users/' + login)
-  res.send(topcreator)
+app.get('/c/:id', (req, res) => {
+  res.send(require('./database/collection/' + req.params.id))
 })
+
+// app.get('/i/:id', (req, res) => {
+//   res.send(require('./database/img/' + req.params.id + '.png'))
+// })
+
+app.get('/topcreator', (req, res) => { res.send(require('./database/top/topCreator.json')) })
+app.get('/topcollection', (req, res) => { res.send(require('./database/top/topCollection.json')) })
 
 app.get('/:user', (req, res) => {
   const path = './database/users/' + req.params.user + '.json'
