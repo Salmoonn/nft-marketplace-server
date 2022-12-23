d61aab7a80e4b0eee8b2564fb7ed4700d002d54e
Salmoonn <qwersapego@gmail.com>
2022-12-23 09:24:39 +0300

    add upload

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const fs = require('fs');
-
 
 const app = express();
 
 app.use(cors());
+app.use(fileUpload());
 
 app.use('/i', express.static('database/img'));
 app.use('/a', express.static('database/avatars'));
@@ -13,6 +14,45 @@ app.use('/other', express.static('database/other'));
 app.use('/placeholder', express.static('database/placeholder'));
 app.use('/d', express.static('database/description'));
 
+
+app.post('/add', (req, res) => {
+
+  path = 'database/'
+
+  if (req.body.name && req.body.author && req.files.image) {
+    const item = {
+      id: (Math.floor(Math.random() * 65536)).toString(16),
+      creator: req.body.author,
+      name: req.body.name,
+      price: Math.floor(Math.random() * 1000) / 100,
+      highestBid: Math.floor(Math.random() * 1000) / 100,
+      tags: [],
+      collection: []
+    }
+
+    if (!fs.existsSync(path + 'users/' + req.body.author + '.json')) {
+      res.status(404)
+      res.send('not found user')
+    }
+
+    fs.writeFileSync('database/unit/' + item.id + '.json', JSON.stringify(item))
+
+    const user = JSON.parse(fs.readFileSync('database/users/' + req.body.author + '.json'))
+    user.item.unshift(item.id)
+    fs.writeFileSync('database/users/' + req.body.author + '.json', JSON.stringify(user))
+
+    fs.writeFileSync('database/img/' + item.id + '.png', req.files.image.data)
+
+    const units = JSON.parse(fs.readFileSync('database/AllUnits.json'))
+    units.unshift(item)
+    fs.writeFileSync('database/AllUnits.json', JSON.stringify(units))
+
+    res.send('OK')
+
+  }
+  res.status(400)
+  res.send('error')
+})
 
 app.get('/favicon.ico', (req, res) => res.end())
 
@@ -43,5 +83,6 @@ app.get('/:user', (req, res) => {
       : req.params.user + ': not found'
   )
 })
+
 
 app.listen(8000, (() => console.log('Start server')))
